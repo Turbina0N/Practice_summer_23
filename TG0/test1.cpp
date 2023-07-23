b119448e6bc20c9fb8c1422bd1ae307ee7d5ea1b
Turbina0N <140111603+Turbina0N@users.noreply.github.com>
2023-07-23 18:50:20 +0300

    Update test1.cpp

--- TG0/test1.cpp
+++ TG0/test1.cpp
@@ -20,18 +20,19 @@ static char orderNew[19] = { -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
 std::vector<char> CreateFile(int process_id, int world_size) {
     srand(time(NULL) ^ process_id);
 
-    std::vector<int> entry(19, 0);
-    std::vector<char> symbols;
+   std::vector<int> entry(19, 0);
+    std::vector<wchar_t> symbols;
 
     int symbols_per_process = N / world_size;
     int remainder = N % world_size;
+
     if (process_id < remainder) {
         symbols_per_process++;
     }
 
     for (int i = 0; i < symbols_per_process; ++i) {
         int index = rand() % 19;
-        char symbol = arr[index];
+        wchar_t symbol = arr[index];
         entry[index]++;
         symbols.push_back(symbol);
     }
