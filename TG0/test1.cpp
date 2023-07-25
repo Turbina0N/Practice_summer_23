234d9c68530d2f9cbdfa0f7df71f4c081c8ec7e9
Turbina0N <140111603+Turbina0N@users.noreply.github.com>
2023-07-25 12:15:42 +0300

    Update test1.cpp

--- TG0/test1.cpp
+++ TG0/test1.cpp
@@ -140,7 +140,11 @@ vector<double> compute_probabilities(const vector<char>& symbols,const vector<ch
     return probabilities;
 }
 std::vector<std::vector<int>> transform_to_rectangle(const std::vector<std::vector<int>>& C) {
-    std::vector<std::vector<int>> C_rectangular(C.size(), std::vector<int>(5, -1));
+	int max_l=0;  
+	for (size_t i = 0; i < C.size(); ++i) {
+        if (C[i].size() > max_l) max_l = C[i].size()
+    }
+    std::vector<std::vector<int>> C_rectangular(C.size(), std::vector<int>(max_l, -1));
     for (size_t i = 0; i < C.size(); ++i) {
         copy(C[i].begin(), C[i].end(), C_rectangular[i].begin());
     }
