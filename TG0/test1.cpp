a3e229ae388128c98997e4ee9911525105f38cef
Turbina0N <140111603+Turbina0N@users.noreply.github.com>
2023-07-26 19:00:35 +0700

    Update test1.cpp

--- TG0/test1.cpp
+++ TG0/test1.cpp
@@ -490,18 +490,18 @@ int main(int argc, char** argv) {
     std::vector<char> chunk(symbols_per_process);
 
     if (world_rank == 0) {
-    //     chunk.assign(file_RLE.begin() + start_symbol, file_RLE.begin() + start_symbol + symbols_per_process);
-    //     for (int i = 1; i < world_size; i++) {
-    //         int start_symbol_i = i * base_process + std::min(i, remainder);
-    //         int symbols_per_process_i = base_process + (i < remainder ? 1 : 0);
-    //         MPI_Status status;
-    //         if (MPI_Send(file_RLE.data() + start_symbol_i, symbols_per_process_i, MPI_CHAR, i, 0, MPI_COMM_WORLD) != MPI_SUCCESS) {
-    //             char err_string[MPI_MAX_ERROR_STRING];
-    //             int len;
-    //             MPI_Error_string(status.MPI_ERROR, err_string, &len);
-    //             printf("MPI_Send failed with error: %s\n", err_string);
-    //         }
-    //     }
+        chunk.assign(file_RLE.begin() + start_symbol, file_RLE.begin() + start_symbol + symbols_per_process);
+        for (int i = 1; i < world_size; i++) {
+            int start_symbol_i = i * base_process + std::min(i, remainder);
+            int symbols_per_process_i = base_process + (i < remainder ? 1 : 0);
+            MPI_Status status;
+            if (MPI_Send(file_RLE.data() + start_symbol_i, symbols_per_process_i, MPI_CHAR, i, 0, MPI_COMM_WORLD) != MPI_SUCCESS) {
+                char err_string[MPI_MAX_ERROR_STRING];
+                int len;
+                MPI_Error_string(status.MPI_ERROR, err_string, &len);
+                printf("MPI_Send failed with error: %s\n", err_string);
+            }
+        }
     } else {
         // MPI_Status status;
         // if (MPI_Recv(chunk.data(), chunk.size(), MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status) != MPI_SUCCESS) {
