8b37f889533f24155251eecdf1df120d02de0a2f
Turbina0N <140111603+Turbina0N@users.noreply.github.com>
2023-07-26 15:15:00 +0700

    Update test1.cpp

--- TG0/test1.cpp
+++ TG0/test1.cpp
@@ -310,12 +310,13 @@ int main(int argc, char** argv) {
         MPI_Finalize();
         return 1;
     }
-    std::vector<char> symbols = CreateFile(alphabet, world_rank, world_size);
-    std::cout <<"rank = "<< world_rank << "\t"<<std::endl;
-    for (auto c: symbols){
-	    std::cout << c;
-    }
-	std::cout<<std::endl;
+
+ //    std::vector<char> symbols = CreateFile(alphabet, world_rank, world_size);
+ //    std::cout <<"rank = "<< world_rank << "\t"<<std::endl;
+ //    for (auto c: symbols){
+	//     std::cout << c;
+ //    }
+	// std::cout<<std::endl;
  //    int numRows = 0;
  //    int numCols = 0;
  //    std::vector<std::vector<int>> C_rectangular;
