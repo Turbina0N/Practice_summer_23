215e0d47f3b3c373ffea5376c826c7ab6caf8d9b
Turbina0N <140111603+Turbina0N@users.noreply.github.com>
2023-07-26 15:08:25 +0700

    Update test1.cpp

--- TG0/test1.cpp
+++ TG0/test1.cpp
@@ -311,7 +311,7 @@ int main(int argc, char** argv) {
         return 1;
     }
     std::vector<char> symbols = CreateFile(alphabet, world_rank, world_size);
-	
+    std::cout <<"rank = "<< world_rank << "\t"<< symbols <<std::endl;
  //    int numRows = 0;
  //    int numCols = 0;
  //    std::vector<std::vector<int>> C_rectangular;
