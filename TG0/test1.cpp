b8ca060cffddaaa7e30ad7d381d020cbfeebfee1
Turbina0N <140111603+Turbina0N@users.noreply.github.com>
2023-07-27 14:45:50 +0700

    Update test1.cpp

--- TG0/test1.cpp
+++ TG0/test1.cpp
@@ -425,7 +425,7 @@ int main(int argc, char** argv) {
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     //std::vector<char> alphabet = load_alphabet("symbols.txt");
-    std::vector<char> alphabet = { 'q', 'w', 'e', 'r', 't', 'y', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '.' };
+    std::vector<char> alphabet = { 'a', 's', 'd', 'f', 'g', 'h', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '.' };
     std::cout <<alphabet.size();
     if (alphabet.empty()) {
         MPI_Finalize();
